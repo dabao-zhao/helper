f06196cc4e04abf1072c35b134f15e2988e1f1af
dabao-zhao <zhao.jiaxing@qq.com>
2023-05-23 10:50:33 +0800

    values 方法和测试

--- map/map.go
+++ map/map.go
@@ -1,5 +1,6 @@
 package _map
 
+// Keys 获取 map 的所有 key
 func Keys[K comparable, V any](m map[K]V) []K {
 	l := len(m)
 	if l == 0 {
@@ -10,6 +11,23 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	keys := make([]K, l)
 	for key := range m {
 		keys[i] = key
+		i++
+	}
+
+	return keys
+}
+
+// Values 获取 map 的所有 value
+func Values[K comparable, V any](m map[K]V) []V {
+	l := len(m)
+	if l == 0 {
+		return nil
+	}
+
+	i := 0
+	keys := make([]V, l)
+	for _, value := range m {
+		keys[i] = value
 		i++
 	}
 
---
+++ map/map_test.go
@@ -0,0 +1,70 @@
+package _map
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var keysTests = []struct {
+	s        map[int64]struct{}
+	expected []int64
+}{
+	{
+		nil,
+		nil,
+	},
+	{
+		map[int64]struct{}{
+			1: {},
+			2: {},
+			3: {},
+		},
+		[]int64{1, 2, 3},
+	},
+}
+
+func TestKeys(t *testing.T) {
+	for _, test := range keysTests {
+		t.Run("", func(t *testing.T) {
+			values := Keys(test.s)
+			// map 是无序的，要先排序再对比
+			sort.Slice(values, func(i, j int) bool {
+				return values[i] < values[j]
+			})
+			assert.Equal(t, test.expected, values)
+		})
+	}
+}
+
+var valuesTests = []struct {
+	s        map[int64]int64
+	expected []int64
+}{
+	{
+		nil,
+		nil,
+	},
+	{
+		map[int64]int64{
+			1: 1,
+			2: 2,
+			3: 3,
+		},
+		[]int64{1, 2, 3},
+	},
+}
+
+func TestValues(t *testing.T) {
+	for _, test := range valuesTests {
+		t.Run("", func(t *testing.T) {
+			values := Values(test.s)
+			// map 是无序的，要先排序再对比
+			sort.Slice(values, func(i, j int) bool {
+				return values[i] < values[j]
+			})
+			assert.Equal(t, test.expected, values)
+		})
+	}
+}
