0ce82f680cbdd63e27303b1e842f3872607d5d5f
dabao-zhao <zhao.jiaxing@qq.com>
2023-05-31 16:53:20 +0800

    md5 相关

---
+++ md5/md5.go
@@ -0,0 +1,30 @@
+package md5
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"os"
+)
+
+func String(str string) (string, error) {
+	h := md5.New()
+	_, err := h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil)), err
+}
+
+func File(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+	return string(h.Sum(nil)), nil
+}
---
+++ md5/md5_test.go
@@ -0,0 +1,25 @@
+package md5
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestString(t *testing.T) {
+	m, err := String("helper")
+	assert.Nil(t, err)
+	assert.Equal(t, "fde5d67bfb6dc4b598291cc2ce35ee4a", m)
+}
+
+func TestFile(t *testing.T) {
+	_, err := os.Create("fde5d67bfb6dc4b598291cc2ce35ee4a")
+	assert.Nil(t, err)
+
+	m, err := File("fde5d67bfb6dc4b598291cc2ce35ee4a")
+	assert.Nil(t, err)
+	assert.Equal(t, "\xd4\x1d\x8cُ\x00\xb2\x04\xe9\x80\t\x98\xec\xf8B~", m)
+
+	_ = os.Remove("fde5d67bfb6dc4b598291cc2ce35ee4a")
+}
