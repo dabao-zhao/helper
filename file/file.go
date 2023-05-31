e8f063826b293ec4f234a7e1ad80de7196d5252f
dabao-zhao <zhao.jiaxing@qq.com>
2023-05-31 16:26:57 +0800

    file 增加几个方法

--- file/file.go
+++ file/file.go
@@ -1,6 +1,9 @@
 package xfile
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func IsDir(path string) bool {
 	var (
@@ -26,4 +29,31 @@ func IsFile(path string) bool {
 		return false
 	}
 	return !fileInfo.IsDir()
+}
+
+func FileExists(file string) bool {
+	_, err := os.Stat(file)
+	return err == nil
+}
+
+func CreateIfNotExist(file string) (*os.File, error) {
+	if _, err := os.Stat(file); !os.IsNotExist(err) {
+		return nil, fmt.Errorf("%s already exist", file)
+	}
+
+	return os.Create(file)
+}
+
+func SameFile(path1, path2 string) (bool, error) {
+	stat1, err := os.Stat(path1)
+	if err != nil {
+		return false, err
+	}
+
+	stat2, err := os.Stat(path2)
+	if err != nil {
+		return false, err
+	}
+
+	return os.SameFile(stat1, stat2), nil
 }
--- file/file_test.go
+++ file/file_test.go
@@ -1,6 +1,8 @@
 package xfile
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +16,30 @@ func TestIsFile(t *testing.T) {
 func TestIsDir(t *testing.T) {
 	assert.Equal(t, false, IsDir("file.go"))
 	assert.Equal(t, true, IsDir("../file"))
+}
+
+func TestFileExists(t *testing.T) {
+	assert.Equal(t, false, FileExists("5BaWY92yLs9K4Fvjk5tRbG9TBojbqlnm.go"))
+	assert.Equal(t, true, FileExists("file.go"))
+}
+
+func TestCreateIfNotExist(t *testing.T) {
+	_, err := CreateIfNotExist("file.go")
+	assert.Equal(t, errors.New("file.go already exist"), err)
+
+	f, err := CreateIfNotExist("OJliXubJw02Q5kpZKyXoeanynKGlVxSb.go")
+	assert.Nil(t, err)
+	assert.Equal(t, "OJliXubJw02Q5kpZKyXoeanynKGlVxSb.go", f.Name())
+
+	os.Remove(f.Name())
+}
+
+func TestSameFile(t *testing.T) {
+	r, err := SameFile("file.go", "file.go")
+	assert.Nil(t, err)
+	assert.Equal(t, true, r)
+
+	r, err = SameFile("file.go", "file_test.go")
+	assert.Nil(t, err)
+	assert.Equal(t, false, r)
 }
