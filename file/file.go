7fe78f83162d20895e73574ed6d0199893cf656e
dabao-zhao <zhao,jiaxing@qq.com>
2023-06-13 16:47:28 +0800

    file&dir copy

--- file/file.go
+++ file/file.go
@@ -2,7 +2,9 @@ package xfile
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func IsDir(path string) bool {
@@ -56,4 +58,105 @@ func SameFile(path1, path2 string) (bool, error) {
 	}
 
 	return os.SameFile(stat1, stat2), nil
+}
+
+func CopyFile(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer func(in *os.File) {
+		if e := in.Close(); e != nil {
+			err = e
+		}
+	}(in)
+
+	if err = os.MkdirAll(filepath.Dir(dst), os.ModePerm); err != nil {
+		return
+	}
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func(out *os.File) {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}(out)
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	stat, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	err = os.Chmod(dst, stat.Mode())
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func CopyDir(src string, dst string) (err error) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	stat, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	if !stat.IsDir() {
+		return fmt.Errorf("source must be a directory")
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+	if err == nil {
+		return fmt.Errorf("destination already exists")
+	}
+
+	err = os.MkdirAll(dst, stat.Mode())
+	if err != nil {
+		return
+	}
+
+	items, err := os.ReadDir(src)
+	if err != nil {
+		return
+	}
+
+	for _, item := range items {
+		srcPath := filepath.Join(src, item.Name())
+		dstPath := filepath.Join(dst, item.Name())
+
+		if item.IsDir() {
+			err = CopyDir(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		} else {
+			i, _ := item.Info()
+			if i.Mode()&os.ModeSymlink != 0 {
+				continue
+			}
+
+			err = CopyFile(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
 }
